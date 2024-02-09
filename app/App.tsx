85d12b61da9f3df73eb164af33aff05625f2a79a
yanquielarango <yanquiel.arango@gmail.com>
2024-02-09 14:56:57 +0100

    creating a custom button

---
+++ app/App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
+
+const PlaceholderImage = require('@assets/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image}/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  image:{
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
